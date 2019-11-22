07758f442c7e505b348f8589e2f7ddd9c2e38972
Pravesha <p.s.p.ramsundersingh@student.tudelft.nl>
2019-11-22 15:20:18 +0100

    added comments to the basis methods

--- src/main/java/Ship.java
+++ src/main/java/Ship.java
@@ -3,13 +3,22 @@ public class Ship {
     public boolean orientation = true;
     public int damage;
 
+    /**
+     * Constructor for the ship class
+     *
+     * @param typeShip the type of ship (dependent on squares)
+     * @param orientation horizontal or vertical ship
+     * @param damage represents how many squares have been hit
+     */
     public Ship(int typeShip, boolean orientation, int damage){
         this.typeShip = typeShip;
         this.orientation = orientation;
         this.damage = damage;
     }
 
-
+    /**
+     * Method for when a ship is hit by the opponent
+     */
     public void hitShip(){
         damage--;
     }
