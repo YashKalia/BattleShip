8a317605b27395090786e667f49be76168cdc3ba
glennvandenbel <g.vandenbelt@student.tudelft.nl>
2019-12-06 16:36:00 +0100

    intelligence computer with small bugs and checkstyle errors

--- src/main/java/OpponentPlayer.java
+++ src/main/java/OpponentPlayer.java
@@ -1,12 +1,34 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class OpponentPlayer {
-    private transient Random random = new Random();
+    private transient Random random;
+    private transient Random random2;
+    private transient Random random3;
+    private transient Random random4;
+    private transient Random random5;
+    private transient Random random6;
+    private transient Random random7;
+    private transient Random random8;
+    private transient Random random9;
+    private transient Random random10;
+    public static ArrayList<Square> shotSquares = new ArrayList<Square>();
 
+    /**
+     * Randomised shot that enemy performs.
+     *
+     * @param playerBoard board of player.
+     */
     protected void enemyShot(Board playerBoard) {
         while (HelloWorld.opponentTurn) {
 
+            if (!shotSquares.isEmpty()) {
+                enemyShotCoordinates(playerBoard, shotSquares.get(0).getCoordinateX(),
+                        shotSquares.get(0).getCoordinateY());
+                break;
+            }
+            random = new Random();
             int x = random.nextInt(10);
             int y = random.nextInt(10);
 
@@ -17,7 +39,11 @@ public class OpponentPlayer {
 
             HelloWorld.opponentTurn = square.shootEnemy();
             if (HelloWorld.opponentTurn) {
-                enemyShot(playerBoard);
+                shotSquares.add(square);
+                if (!square.getShip().isNotDestroyed()) {
+                    shotSquares.clear();
+                }
+                enemyShotCoordinates(playerBoard, x, y);
             }
 
             if (playerBoard.ships == 0) {
@@ -26,8 +52,295 @@ public class OpponentPlayer {
 
             }
         }
+
     }
 
+    /**
+     * Shot that enemy performs, but not randomised.
+     *
+     * @param playerBoard board of player.
+     * @param x           coordinate of square location
+     * @param y           coordinate of square location
+     */
+    protected void enemyShotCoordinates(Board playerBoard, int x, int y) {
+        while (HelloWorld.opponentTurn) {
+
+            random2 = new Random();
+            int random = random2.nextInt(4);
+            switch (random) {
+                case 0:
+                    shootRight(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootLeft(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootDown(playerBoard, x, y);
+                    break;
+                case 3:
+                    shootUp(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+
+        if (playerBoard.ships == 0) {
+            System.out.println("YOU LOSE");
+            // System.exit(0);
+
+        }
+    }
+
+    /**
+     * Shoot on the square above the locations given.
+     *
+     * @param playerBoard board of the player
+     * @param x           coordinate of square location
+     * @param y           coordinate of square location
+     */
+    public void shootUp(Board playerBoard, int x, int y) {
+
+        if (y == 0) {
+            random3 = new Random();
+            int random = random3.nextInt(4);
+            switch (random) {
+                case 0:
+                    shootRight(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootLeft(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootDown(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+        Square square = playerBoard.getSquare(x, y - 1);
+
+        if (square.shooted || (!playerBoard.inRange(square.getCoordinateX(), square.getCoordinateY()))) {
+            random4 = new Random();
+            int random = random4.nextInt(3);
+            switch (random) {
+                case 0:
+                    shootRight(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootLeft(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootDown(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+
+            }
+        }
+        HelloWorld.opponentTurn = square.shootEnemy();
+        if (HelloWorld.opponentTurn) {
+            shotSquares.add(square);
+            if (!square.getShip().isNotDestroyed()) {
+                shotSquares.clear();
+            }
+            shootUp(playerBoard, square.getCoordinateX(), square.getCoordinateY());
+        } else {
+            enemyShot(playerBoard);
+        }
+    }
+
+
+    /**
+     * Shoot on the square left the locations given.
+     *
+     * @param playerBoard board of the player
+     * @param x           coordinate of square location
+     * @param y           coordinate of square location
+     */
+    public void shootLeft(Board playerBoard, int x, int y) {
+
+        System.out.println(x);
+        if (x == 0) {
+            random5 = new Random();
+            int random = random5.nextInt(3);
+            switch (random) {
+                case 0:
+                    shootRight(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootUp(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootDown(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+        Square square = playerBoard.getSquare(x - 1, y);
+
+        if (square.shooted || (!playerBoard.inRange(square.getCoordinateX(), square.getCoordinateY()))) {
+            random6 = new Random();
+            int random = random6.nextInt(3);
+            switch (random) {
+                case 0:
+                    shootUp(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootDown(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootRight(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+        HelloWorld.opponentTurn = square.shootEnemy();
+        if (HelloWorld.opponentTurn) {
+            shotSquares.add(square);
+            if (!square.getShip().isNotDestroyed()) {
+                shotSquares.clear();
+            }
+            shootLeft(playerBoard, square.getCoordinateX(), square.getCoordinateY());
+        } else {
+            enemyShot(playerBoard);
+        }
+    }
+
+    /**
+     * Shoot on the square right the locations given.
+     *
+     * @param playerBoard board of the player
+     * @param x           coordinate of square location
+     * @param y           coordinate of square location
+     */
+    public void shootRight(Board playerBoard, int x, int y) {
+
+        int nine = 9; //PMD purposes.
+
+        if (x == nine) {
+            random7 = new Random();
+            int random = random7.nextInt(3);
+            switch (random) {
+                case 0:
+                    shootUp(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootLeft(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootDown(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+        Square square = playerBoard.getSquare(x + 1, y);
+
+        if (square.shooted || (!playerBoard.inRange(square.getCoordinateX(), square.getCoordinateY()))) {
+            random8 = new Random();
+            int random = random8.nextInt(3);
+            switch (random) {
+                case 0:
+                    shootUp(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootLeft(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootDown(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+        HelloWorld.opponentTurn = square.shootEnemy();
+        if (HelloWorld.opponentTurn) {
+            shotSquares.add(square);
+            if (!square.getShip().isNotDestroyed()) {
+                shotSquares.clear();
+            }
+            shootRight(playerBoard, square.getCoordinateX(), square.getCoordinateY());
+        } else {
+            enemyShot(playerBoard);
+        }
+    }
+
+
+    /**
+     * Shoot on the square down the locations given.
+     *
+     * @param playerBoard board of the player
+     * @param x           coordinate of square location
+     * @param y           coordinate of square location
+     */
+    public void shootDown(Board playerBoard, int x, int y) {
+
+        int nine = 9; //PMD purposes.
+        if (y == nine) {
+            random9 = new Random();
+            int random = random9.nextInt(3);
+            switch (random) {
+                case 0:
+                    shootRight(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootLeft(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootUp(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+        Square square = playerBoard.getSquare(x, y + 1);
+
+        if (square.shooted || (!playerBoard.inRange(square.getCoordinateX(), square.getCoordinateY()))) {
+            random10 = new Random();
+            int random = random10.nextInt(3);
+            switch (random) {
+                case 0:
+                    shootRight(playerBoard, x, y);
+                    break;
+                case 1:
+                    shootLeft(playerBoard, x, y);
+                    break;
+                case 2:
+                    shootUp(playerBoard, x, y);
+                    break;
+                default:
+                    enemyShotCoordinates(playerBoard, x, y);
+                    break;
+            }
+        }
+        HelloWorld.opponentTurn = square.shootEnemy();
+        if (HelloWorld.opponentTurn) {
+            shotSquares.add(square);
+            if (!square.getShip().isNotDestroyed()) {
+                shotSquares.clear();
+            }
+            shootDown(playerBoard, square.getCoordinateX(), square.getCoordinateY());
+        } else {
+            enemyShot(playerBoard);
+        }
+    }
+
+    /**
+     * method to place the opponent ships.
+     *
+     * @param opponentBoard board of the opponent.
+     */
     @SuppressWarnings("PMD")
     protected void placeShipsOpponent(Board opponentBoard) {
         List<Ship> ships = Board.makeListWithShips();
@@ -35,8 +348,10 @@ public class OpponentPlayer {
         int allShipsPlaced = 4;
         while (!ships.isEmpty()) {
 
-            int x = random.nextInt(10);
-            int y = random.nextInt(10);
+
+            random10 = new Random();
+            int x = random10.nextInt(10);
+            int y = random10.nextInt(10);
 
             if (opponentBoard.placeShip(ships.get(allShipsPlaced), x, y)) {
                 allShipsPlaced--;
