3c83b228bf8bd85414cb9a07fcb3eccbb6bc31fa
Yash Kalia <Y.Kalia@student.tudelft.nl>
2019-12-13 18:49:48 +0100

    Made start screen

---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,40 @@
+package gui;
+
+import java.io.File;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.stage.Stage;
+
+
+
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        File file = new File("src/main/java/gui/fxml/MainFXML.fxml");
+        primaryStage.setTitle("BattleShip");
+        URL url = file.toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+        //scene.getStylesheets().add(getClass()
+        // .getResource("application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
+        primaryStage.show();
+
+    }
+
+}
