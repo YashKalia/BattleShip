9b29a6bfe66cc00849d6e273df8165a51c73acf2
mahira_a <m.ali-10@student.tudelft.nl>
2019-12-20 11:11:02 +0100

    Attempt to fix gui

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.File;
 import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,27 +11,20 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        File file = new File("src/main/gui/fxml/MainFXML.fxml");
+        File file = new File("src/main/java/gui/fxml/MainFXML.fxml");
         primaryStage.setTitle("BattleShip");
         URL url = file.toURI().toURL();
-        FXMLLoader loader = new FXMLLoader(url);
-        Parent root = loader.load();
+        Parent root = FXMLLoader.load(url);
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.sizeToScene();
         primaryStage.show();
-
     }
-
-
-
 }
